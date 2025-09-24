303a58cdfb6c5026751f8886b8a5c531d0c616f5
Faruktfk <66157199+Faruktfk@users.noreply.github.com>
2025-09-24 13:43:40 +0200

    ASE-768 Pfad-Ändern implementiert, unnötige Komponente gelöscht.

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import {
 import { THEME_ID as MATERIAL_THEME_ID, ThemeProvider } from '@mui/material';
 import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy';
 import './i18n';
-import { Provider } from 'react-redux';
-import store from '@stores/index.ts';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import 'dayjs/locale/de';
@@ -28,21 +26,19 @@ function App({ basename }: AppProps) {
   }
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
-      <Provider store={store}>
-        <ThemeProvider theme={{ [MATERIAL_THEME_ID]: muiTheme }}>
-          <JoyCssVarsProvider
-            theme={joyTheme}
-            defaultMode="light"
-            modeStorageKey="joy-mode"
-            colorSchemeStorageKey="joy-color-scheme"
-          >
-            <LanguageSelectorComponent />
-            <BrowserRouter basename={basename}>
-              <RoutingComponent />
-            </BrowserRouter>
-          </JoyCssVarsProvider>
-        </ThemeProvider>
-      </Provider>
+      <ThemeProvider theme={{ [MATERIAL_THEME_ID]: muiTheme }}>
+        <JoyCssVarsProvider
+          theme={joyTheme}
+          defaultMode="light"
+          modeStorageKey="joy-mode"
+          colorSchemeStorageKey="joy-color-scheme"
+        >
+          <LanguageSelectorComponent />
+          <BrowserRouter basename={basename}>
+            <RoutingComponent />
+          </BrowserRouter>
+        </JoyCssVarsProvider>
+      </ThemeProvider>
     </LocalizationProvider>
   );
 }
--- src/pages/BachelorAnmeldung/BachelorAnmeldung.tsx
+++ src/pages/BachelorAnmeldung/BachelorAnmeldung.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Dayjs } from 'dayjs';
 import {
   Box,
