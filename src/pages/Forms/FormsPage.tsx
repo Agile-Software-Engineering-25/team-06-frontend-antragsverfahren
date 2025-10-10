cecc512943a41f55895d834d5071f9a00a2dbc46
Cunha Preußler, Luiza <Luiza.Cunha-Preussler@telekom.de>
2025-10-10 10:27:31 +0200

    made Studienbescheinigung responsive
    Therefore, a new component was made

--- src/pages/Forms/FormsPage.tsx
+++ src/pages/Forms/FormsPage.tsx
@@ -4,27 +4,13 @@ import BachelorAnmeldung from '../BachelorAnmeldung/BachelorAnmeldung';
 import { useTranslation } from 'react-i18next';
 import useApiForm from '@/hooks/useApiForm';
 import { Box } from '@mui/joy';
-import { Accordion, Card } from '@agile-software/shared-components';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { Accordion } from '@agile-software/shared-components';
+import StudienbescheinigungCard from '@components/Studienbescheinigung/StudienbescheinigungComponent.tsx';
 
 export default function FormsPage() {
   const { t } = useTranslation();
 
-  const {
-    createNachklausurAntrag,
-    createBachelorAnmeldung,
-    getStudienbescheinigung,
-  } = useApiForm();
-
-  const onStudienbescheinigung = async (): Promise<void> => {
-    const data = await getStudienbescheinigung();
-    if (data) {
-      const url = URL.createObjectURL(data);
-      window.open(url, '_blank', 'noopener,noreferrer');
-    } else {
-      alert(t('pages.forms.studienbescheinigung.error'));
-    }
-  };
+  const { createNachklausurAntrag, createBachelorAnmeldung } = useApiForm();
 
   const accordionItems = [
     {
@@ -53,40 +39,7 @@ export default function FormsPage() {
         gap: 3,
       }}
     >
-      <Card
-        title={t('pages.forms.studienbescheinigung.title')}
-        titleSX={{
-          fontSize: { xs: '1rem', sm: '1.2rem', md: '1.5rem' },
-        }}
-        imageButton={{
-          text: (
-            <>
-              <FileDownloadIcon
-                sx={{
-                  fontSize: { xs: 16, sm: 20, md: 24 },
-                }}
-              />
-              {t('pages.forms.studienbescheinigung.buttonLabel')}
-            </>
-          ),
-          onClick: onStudienbescheinigung,
-          variant: 'solid',
-          color: 'primary',
-          sx: {
-            fontSize: { xs: '0.75rem', sm: '0.875rem', md: '1rem' },
-            px: { xs: 1, sm: 2 },
-            py: { xs: 0.5, sm: 1 },
-          },
-        }}
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', sm: 'row' },
-          width: '100%',
-          p: { xs: 2, sm: 3, md: 4 },
-          alignItems: { xs: 'flex-start', sm: 'center' },
-          gap: { xs: 1, sm: 2 },
-        }}
-      />
+      <StudienbescheinigungCard />
 
       <Accordion
         items={accordionItems}
